bd0ef50f52ff2df3596cf6c8d3391cf84d792965
Agam Darmawan <madridlover085@gmail.com>
2025-10-02 15:04:50 +0700

    konz

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hi, ini baris kode pertama ");
         System.out.println("ini baris kode kedua");
+        System.out.println("ini baris kode ketiga");
     }
 }
