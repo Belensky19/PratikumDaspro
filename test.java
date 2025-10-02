901c8d100b4dfd174de7371eafd9379509bf1a68
Belensky19 <prayogakepanjen@gmail.com>
2025-10-02 15:03:44 +0700

    Mesagge

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("hi, ini baris kode pertama ");
+        System.out.println("hi,ini baris kode pertama");
+        System.out.println("ini baris kode kedua");
+        System.out.println("ini baris kode ketiga");
     }
 }
