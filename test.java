e1eda3356bd442e99d2d22a484fc9bff902f9845
Belensky19 <prayogakepanjen@gmail.com>
2025-10-02 15:20:32 +0700

    Message

--- test.java
+++ test.java
@@ -3,7 +3,7 @@ public class test {
         System.out.println("hi,ini baris kode pertama");
         System.out.println("ini baris kode kedua");
         System.out.println("ini baris kode ketiga");
-        `System.out.println("ini baris kode keempat");
+        System.out.println("ini baris kode keempat");
         System.out.println("ini baris kode kelima");
         System.out.println("ini baris kode keenam");    
         System.out.println("ini baris kode ketujuh");
