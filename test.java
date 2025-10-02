c2c5f99b13853dcf68e5dfc6394fb279f120d60f
Agam Darmawan <madridlover085@gmail.com>
2025-10-02 15:01:50 +0700

    ask;jdkasd

--- test.java
+++ test.java
@@ -1,5 +1,6 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hi, ini baris kode pertama ");
+        System.out.println("ini baris kode kedua");
     }
 }
