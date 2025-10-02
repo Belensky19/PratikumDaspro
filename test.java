6138d353e7ea4784045ef77224d7b36282a1384a
Belensky19 <prayogakepanjen@gmail.com>
2025-10-02 15:32:05 +0700

    R

--- test.java
+++ test.java
@@ -2,12 +2,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hi,ini baris kode pertama");
         System.out.println("ini baris kode kedua");
-        System.out.println("ini baris kode ketiga");
-        System.out.println("ini baris kode keempat");
-        System.out.println("ini baris kode kelima");
-        System.out.println("ini baris kode keenam");    
-        System.out.println("ini baris kode ketujuh");
-        System.out.println("ini baris kode kedelapan");
-        System.out.println("ini baris kode kesembilan");    
+        
     }
 }
